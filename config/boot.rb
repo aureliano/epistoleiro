d7d8dc175261f2004c1adb424854be39e21bbc2e
aureliano <aureliano.franca@hotmail.com>
2014-08-13 09:59:13 -0300

    Load application configuration parameters from file.

--- config/boot.rb
+++ config/boot.rb
@@ -7,6 +7,11 @@ require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, RACK_ENV)
 
+if File.exist? 'config/app-cfg-params.yml'
+  conf_params = YAML.load_file 'config/app-cfg-params.yml'
+  conf_params.each {|key, value| ENV[key.gsub('.', '_').upcase] = value.to_s }
+end
+
 Padrino.before_load do
   I18n.enforce_available_locales = true
   I18n.locale = ENV['APP_LOCATION'].to_sym
