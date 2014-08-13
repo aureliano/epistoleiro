e25d2bf47a098a22a5ecfd59fd20ca461682078f
aureliano <aureliano.franca@hotmail.com>
2014-08-13 09:38:23 -0300

    Bug fix.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,12 @@
 RACK_ENV = 'test' unless defined?(RACK_ENV)
+
+ENV['APP_LOCATION'] = 'en'
+ENV['PASSWORD_HASH_ITERATION_SIZE'] = '10'
+ENV['SALT_NUMBER_SIZE'] = '3'
+
 require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
 
 require 'capybara/cucumber'
 require 'rspec/expectations'
 
-##
-# You can handle all padrino applications using instead:
-#   Padrino.application
 Capybara.app = Epistoleiro::App.tap { |app|  }
